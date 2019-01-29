06802d0ad9f04181c242dcb1e54d0070b045f0b6
Rohit Agrawal <rohitagrawalcgartist@gmail.com>
2019-01-30 02:21:30 +0530

    Start with create-react-app

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import { ToastContainer, toast } from 'react-toastify';
 const request = require('superagent');
 const axios = require('axios')
 class App extends Component {
