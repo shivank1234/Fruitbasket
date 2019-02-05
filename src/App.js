f382942864c4d2f89bb58d3bf879de263ece9ddb
Rohit Agrawal <rohitagrawalcgartist@gmail.com>
2019-02-06 00:50:49 +0530

    Next iter

--- src/App.js
+++ src/App.js
@@ -22,21 +22,11 @@ class App extends Component {
           kiwi: 0,
           total: 0,
           paynow: "Now",
-          method: "PayTM",
-          paid: "False"
+          method: "PayTM"
         }
   }
 
-  IDAdded = (event) => {
-    if(event.target.value.length > 0){
-      this.setState({paid: "True"});
-    }
-    else{
-      this.setState({paid: "False"});
-    }
-  }
-
-  ifPayTM = () => {
+  paymentMethod = () => {
       if(this.state.method == "PayTM"){
       return (
         <div>
@@ -60,12 +50,12 @@ class App extends Component {
         <br />
         <p class="text-center">Pay the above amount to <b>9650999693</b> through PayTM</p>
           <div class="qrcode"></div>
-            <label>PayTM transaction ID</label>
+            <label>Payment transaction ID</label>
             <input name="entry.82500164" type="text" class="form-control" onChange={this.IDAdded} required></input>
         </div>
       );
     }
-    else{
+    else if(this.state.method == "Bhim"){
       return (
         <div>
         <br/>
@@ -78,42 +68,53 @@ class App extends Component {
           </div>
         </div>
         <br /><br/>
-        <p class="text-center">Pay the above amount to <b>9650999693@paytm</b> through UPI</p>
+        <p class="text-center">Pay the above amount to <b>9650999693@paytm</b> through Bhim</p>
         <br />
           <div class="upicode"></div>
-            <label>UPI Transaction ID</label>
-            <input name="entry.1068253630" type="text" class="form-control" onChange={this.IDAdded} required></input>
+            <label>Payment Transaction ID</label>
+            <input name="entry.82500164" type="text" class="form-control" onChange={this.IDAdded} required></input>
         </div>
       );
     }
-  }
-
-  ifPayNow = () => {
-      if(this.state.paynow == "Now"){
+    else if(this.state.method == "PhonePe"){
       return (
         <div>
-          <br />
-          <div class="form-group">
-            <label>Payment Method</label>
-            <div class="select">
-            <select class="form-control" onChange={this.getMethod} id="exampleFormControlSelect1">
-              <option>PayTM</option>
-              <option>UPI</option>
-            </select>
-            </div>
+        <br/>
+        <div class="row">
+          <div class="col">
+            <h2 class="text-center">Final Price: </h2>
           </div>
-          <br />
-          {this.ifPayTM()}
-          <br />
+          <div class="col">
+          <h2 class="text-center">Rs. {this.TotalPrice()}</h2>
+          </div>
+        </div>
+        <br /><br/>
+        <p class="text-center">Pay the above amount to <b>9650999693</b> through PhonePe</p>
+        <br />
+          <div class="phonepecode"></div>
+            <label>Payment Transaction ID</label>
+            <input name="entry.82500164" type="text" class="form-control" onChange={this.IDAdded} required></input>
         </div>
       );
     }
-    else{
+    else if(this.state.method == "Tez"){
       return (
         <div>
-        <p><b>Disclaimer: Your order is not yet confirmed! Kindly PayTM the money to 9650999693 to complete your order</b></p>
-        <input name="entry.82500164" value="None" style={{'display': "none"}}></input>
+        <br/>
+        <div class="row">
+          <div class="col">
+            <h2 class="text-center">Final Price: </h2>
+          </div>
+          <div class="col">
+          <h2 class="text-center">Rs. {this.TotalPrice()}</h2>
+          </div>
+        </div>
+        <br /><br/>
+        <p class="text-center">Pay the above amount to <b>9650999693</b> through Tez</p>
         <br />
+          <div class="tezcode"></div>
+            <label>Payment Transaction ID</label>
+            <input name="entry.82500164" type="text" class="form-control" onChange={this.IDAdded} required></input>
         </div>
       );
     }
@@ -122,33 +123,57 @@ class App extends Component {
   bananaPrice = () => {
     return this.state.bananas * 33;
   }
+  bananaQty = () => {
+    return this.state.bananas * 6;
+  }
   applePrice = () => {
     return this.state.apples * 55;
+  }
+  appleQty = () => {
+    return Math.floor(this.state.apples * 2.5);
   }
   GrapesBPrice = () => {
     return this.state.grapes_b * 75;
   }
+  GrapesBQty = () => {
+    return (this.state.grapes_b * 0.5);
+  }
   GrapesGPrice = () => {
     return this.state.grapes_g * 60;
+  }
+  GrapesGQty = () => {
+    return (this.state.grapes_g * 0.5);
   }
   KinuPrice = () => {
     return this.state.kinu * 40;
   }
+  KinuQty = () => {
+    return this.state.kinu * 5;
+  }
   GuavaPrice = () => {
     return this.state.guava * 60;
+  }
+  GuavaQty = () => {
+    return this.state.guava * 5;
   }
   StrawberryPrice = () => {
     return this.state.strawberry * 90;
   }
+  StrawberryQty = () => {
+    return this.state.strawberry;
+  }
   KiwiPrice = () => {
     return this.state.kiwi * 25;
+  }
+  KiwiQty = () => {
+    return this.state.kiwi;
   }
   TotalPrice = () => {
     return this.bananaPrice() + this.applePrice() + this.GrapesBPrice() + this.GrapesGPrice() + this.KinuPrice() + this.GuavaPrice() + this.StrawberryPrice() + this.KiwiPrice();
   }
   CumTotalPrice = () => {
     if(this.state.method == "PayTM"){
-    return this.TotalPrice() + this.TaxPrice();
+    return Math.ceil(this.TotalPrice() + this.TaxPrice());
   }
   else{
     return this.TotalPrice();
@@ -230,19 +255,19 @@ class App extends Component {
           <form action="https://docs.google.com/forms/d/e/1FAIpQLSeK8sYEwxuSmUiRxapBaZvq7Kqf7SZAmfxMmUxd74UfLFtZFA/formResponse" target="_self" method="POST">
             <div class="form-group">
               <label for="exampleInputEmail1">Name</label>
-              <input name='entry.1959619863' type="text" class="form-control" placeholder="Enter name" onChange={this.getName}></input>
+              <input name='entry.1959619863' type="text" class="form-control" placeholder="Enter name" onChange={this.getName} required></input>
             </div>
             <div class="form-group">
               <label for="exampleInputEmail1">SNU Roll Number</label>
-              <input name='entry.1612400995' type="text" class="form-control" placeholder="Enter ID" onChange={this.getID}></input>
+              <input name='entry.1612400995' type="text" class="form-control" placeholder="Enter ID" onChange={this.getID} required></input>
             </div>
             <div class="form-group">
               <label for="exampleInputEmail1">SNU Email ID</label>
-              <input name='entry.229529405' type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" onChange={this.getEmail}></input>
+              <input name='entry.229529405' type="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email" onChange={this.getEmail} required></input>
             </div>
             <div class="form-group">
-              <label for="exampleInputEmail1">Contact Number</label>
-              <input name='entry.2086667668' type="text" class="form-control" placeholder="Enter Phone Number" onChange={this.getNo}></input>
+              <label for="exampleInputEmail1">Contact Number (Calling Number)</label>
+              <input name='entry.2086667668' type="text" class="form-control" placeholder="Enter Phone Number" onChange={this.getNo} required></input>
             </div>
             <div class="form-group">
               <label>Hostel</label>
@@ -268,10 +293,22 @@ class App extends Component {
                 <p class="text-center">Bananas (Rs 33/half dozen)</p>
               </div>
               <div class='col-xs-6 col-md-4'>
-                <input name='entry.279668502' type="text" value={this.state.bananas} class="form-control mx-auto" placeholder="qty" style={{"width":"50%"}} onChange={this.getbananas}></input>
+                <select class="form-control" id="exampleFormControlSelect1" value={this.state.bananas} style={{"width":"50%","display":"block","margin":"auto"}} onChange={this.getbananas}>
+                  <option>0</option>
+                  <option>1</option>
+                  <option>2</option>
+                  <option>3</option>
+                  <option>4</option>
+                  <option>5</option>
+                  <option>6</option>
+                  <option>7</option>
+                  <option>8</option>
+                  <option>9</option>
+                </select>
+              <input name='entry.279668502' style={{'display': "none"}} value={this.bananaQty()}></input>
               </div>
               <div class='col-xs-6 col-md-4'>
-                <h4 class="text-center">Rs. {this.bananaPrice()}</h4>
+                <h4 class="text-center">Rs. {this.bananaPrice()} ({this.bananaQty()} Bananas)</h4>
               </div>
             </div>
 
@@ -280,10 +317,22 @@ class App extends Component {
                 <p class="text-center">Apples (Rs 55/500gm)</p>
               </div>
               <div class='col-xs-6 col-md-4'>
-                <input name='entry.1905506033' type="text" value={this.state.apples} class="form-control mx-auto" placeholder="qty" style={{"width":"50%"}} onChange={this.getapples}></input>
+                <select class="form-control" id="exampleFormControlSelect1" value={this.state.apples} style={{"width":"50%","display":"block","margin":"auto"}} onChange={this.getapples}>
+                  <option>0</option>
+                  <option>1</option>
+                  <option>2</option>
+                  <option>3</option>
+                  <option>4</option>
+                  <option>5</option>
+                  <option>6</option>
+                  <option>7</option>
+                  <option>8</option>
+                  <option>9</option>
+                </select>
+              <input name='entry.1905506033' style={{'display': "none"}} value={this.appleQty()}></input>
               </div>
               <div class='col-xs-6 col-md-4'>
-                <h4 class="text-center">Rs. {this.applePrice()}</h4>
+                <h4 class="text-center">Rs. {this.applePrice()} ({this.appleQty()} Apples)</h4>
               </div>
             </div>
 
@@ -292,10 +341,21 @@ class App extends Component {
                 <p class="text-center">Black Grapes (Rs 75/500gm)</p>
               </div>
               <div class='col-xs-6 col-md-4'>
-                <input name='entry.578303012' type="text" value={this.state.grapes_b} class="form-control mx-auto" placeholder="qty" style={{"width":"50%"}} onChange={this.getgrapesB}></input>
+                <select class="form-control" id="exampleFormControlSelect1" name='entry.578303012' value={this.state.grapes_b} style={{"width":"50%","display":"block","margin":"auto"}} onChange={this.getgrapesB}>
+                  <option>0</option>
+                  <option>1</option>
+                  <option>2</option>
+                  <option>3</option>
+                  <option>4</option>
+                  <option>5</option>
+                  <option>6</option>
+                  <option>7</option>
+                  <option>8</option>
+                  <option>9</option>
+                </select>
               </div>
               <div class='col-xs-6 col-md-4'>
-                <h4 class="text-center">Rs. {this.GrapesBPrice()}</h4>
+                <h4 class="text-center">Rs. {this.GrapesBPrice()} ({this.GrapesBQty()} Kg)</h4>
               </div>
             </div>
 
@@ -304,10 +364,21 @@ class App extends Component {
                 <p class="text-center">Green Grapes (Rs 60/500gm)</p>
               </div>
               <div class='col-xs-6 col-md-4'>
-                <input name='entry.650915965' type="text" value={this.state.grapes_g} class="form-control mx-auto" placeholder="qty" style={{"width":"50%"}} onChange={this.getgrapesG}></input>
+                <select class="form-control" id="exampleFormControlSelect1" name='entry.650915965' value={this.state.grapes_g} style={{"width":"50%","display":"block","margin":"auto"}} onChange={this.getgrapesG}>
+                  <option>0</option>
+                  <option>1</option>
+                  <option>2</option>
+                  <option>3</option>
+                  <option>4</option>
+                  <option>5</option>
+                  <option>6</option>
+                  <option>7</option>
+                  <option>8</option>
+                  <option>9</option>
+                </select>
               </div>
               <div class='col-xs-6 col-md-4'>
-                <h4 class="text-center">Rs. {this.GrapesGPrice()}</h4>
+                <h4 class="text-center">Rs. {this.GrapesGPrice()} ({this.GrapesGQty()} Kg)</h4>
               </div>
             </div>
 
@@ -316,10 +387,22 @@ class App extends Component {
                 <p class="text-center">Kinu/Orange (Rs 40/kg)</p>
               </div>
               <div class='col-xs-6 col-md-4'>
-                <input name='entry.1484179781' type="text" value={this.state.kinu} class="form-control mx-auto" placeholder="qty" style={{"width":"50%"}} onChange={this.getKinu}></input>
+                <select class="form-control" id="exampleFormControlSelect1" value={this.state.kinu} style={{"width":"50%","display":"block","margin":"auto"}} onChange={this.getKinu}>
+                  <option>0</option>
+                  <option>1</option>
+                  <option>2</option>
+                  <option>3</option>
+                  <option>4</option>
+                  <option>5</option>
+                  <option>6</option>
+                  <option>7</option>
+                  <option>8</option>
+                  <option>9</option>
+                </select>
+              <input name='entry.1484179781' style={{'display': "none"}} value={this.KinuQty()}></input>
               </div>
               <div class='col-xs-6 col-md-4'>
-                <h4 class="text-center">Rs. {this.KinuPrice()}</h4>
+                <h4 class="text-center">Rs. {this.KinuPrice()} ({this.KinuQty()} Kinu)</h4>
               </div>
             </div>
 
@@ -328,10 +411,22 @@ class App extends Component {
                 <p class="text-center">Guava (Rs 60/kg)</p>
               </div>
               <div class='col-xs-6 col-md-4'>
-                <input name='entry.549511206' type="text" value={this.state.guava} class="form-control mx-auto" placeholder="qty" style={{"width":"50%"}} onChange={this.getGuava}></input>
+                <select class="form-control" id="exampleFormControlSelect1" value={this.state.guava} style={{"width":"50%","display":"block","margin":"auto"}} onChange={this.getGuava}>
+                  <option>0</option>
+                  <option>1</option>
+                  <option>2</option>
+                  <option>3</option>
+                  <option>4</option>
+                  <option>5</option>
+                  <option>6</option>
+                  <option>7</option>
+                  <option>8</option>
+                  <option>9</option>
+                </select>
+              <input name='entry.549511206' style={{'display': "none"}} value={this.GuavaQty()}></input>
               </div>
               <div class='col-xs-6 col-md-4'>
-                <h4 class="text-center">Rs. {this.GuavaPrice()}</h4>
+                <h4 class="text-center">Rs. {this.GuavaPrice()} ({this.GuavaQty()} Guavas)</h4>
               </div>
             </div>
 
@@ -340,10 +435,22 @@ class App extends Component {
                 <p class="text-center">Strawberry (Rs 90/box)</p>
               </div>
               <div class='col-xs-6 col-md-4'>
-                <input name='entry.747644496' type="text" value={this.state.strawberry} class="form-control mx-auto" placeholder="qty" style={{"width":"50%"}} onChange={this.getStrawberry}></input>
+                <select class="form-control" id="exampleFormControlSelect1" value={this.state.strawberry} style={{"width":"50%","display":"block","margin":"auto"}} onChange={this.getStrawberry}>
+                  <option>0</option>
+                  <option>1</option>
+                  <option>2</option>
+                  <option>3</option>
+                  <option>4</option>
+                  <option>5</option>
+                  <option>6</option>
+                  <option>7</option>
+                  <option>8</option>
+                  <option>9</option>
+                </select>
+              <input name='entry.747644496' style={{'display': "none"}} value={this.StrawberryQty()}></input>
               </div>
               <div class='col-xs-6 col-md-4'>
-                <h4 class="text-center">Rs. {this.StrawberryPrice()}</h4>
+                <h4 class="text-center">Rs. {this.StrawberryPrice()} ({this.StrawberryQty()} Box)</h4>
               </div>
             </div>
 
@@ -352,10 +459,22 @@ class App extends Component {
                 <p class="text-center">Kiwi (Rs 25/piece)</p>
               </div>
               <div class='col-xs-6 col-md-4'>
-                <input name='entry.635633533' type="text" value={this.state.kiwi} class="form-control mx-auto" placeholder="qty" style={{"width":"50%"}} onChange={this.getKiwi}></input>
+                <select class="form-control" id="exampleFormControlSelect1" value={this.state.kiwi} style={{"width":"50%","display":"block","margin":"auto"}} onChange={this.getKiwi}>
+                  <option>0</option>
+                  <option>1</option>
+                  <option>2</option>
+                  <option>3</option>
+                  <option>4</option>
+                  <option>5</option>
+                  <option>6</option>
+                  <option>7</option>
+                  <option>8</option>
+                  <option>9</option>
+                </select>
+              <input name='entry.635633533' style={{'display': "none"}} value={this.KiwiQty()}></input>
               </div>
               <div class='col-xs-6 col-md-4'>
-                <h4 class="text-center">Rs. {this.KiwiPrice()}</h4>
+                <h4 class="text-center">Rs. {this.KiwiPrice()} ({this.KiwiQty()} Kiwis)</h4>
               </div>
             </div>
             <br/><br/>
@@ -368,17 +487,25 @@ class App extends Component {
               </div>
             </div>
             <br/><br/>
-            <div class="form-group">
-              <label>Pay Now/Later?</label>
-              <div class="select">
-              <select class="form-control" onChange={this.getPayment} id="exampleFormControlSelect1">
-                <option>Now</option>
-                <option>Later</option>
-              </select>
+
+            <div>
+              <br />
+              <div class="form-group">
+                <label>Payment Method</label>
+                <div class="select">
+                <select class="form-control" onChange={this.getMethod} id="exampleFormControlSelect1">
+                  <option>PayTM</option>
+                  <option>Bhim</option>
+                  <option>PhonePe</option>
+                  <option>Tez</option>
+                </select>
+                </div>
               </div>
+              <br />
+              {this.paymentMethod()}
+              <br />
             </div>
-            {this.ifPayNow()}
-            <input name='entry.1423266867' style={{'display': "none"}} value={this.state.paid}></input>
+
             <input name='entry.369094106' style={{'display': "none"}} value={this.state.method}></input>
             <input name='entry.1069858187' style={{'display': "none"}} value={this.CumTotalPrice()}></input>
             <br />
